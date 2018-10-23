96717d5617cf6db168f00541bcd5328d4421cffc
SugarShootingStar <ryu.8224@gmail.com>
2018-10-23 17:12:17 +0900

    fix:update vue-cli 2.96→3.0.5

--- src/router.js
+++ src/router.js
@@ -1,8 +1,21 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import axios from 'axios';
+import googleMap from '@google/maps';
+import Vuesax from 'vuesax';
+import 'vuesax/dist/vuesax.css';
+import 'material-icons/iconfont/material-icons.css';
+
+Vue.prototype.googleMapsClient = googleMap.createClient({
+  key: process.env.VUE_APP_GOOGLE_API_KEY,
+  Promise: Promise,
+});
+Vue.prototype.$axios = axios;
 
 Vue.use(Router);
+Vue.use(Vuesax);
+
 
 export default new Router({
   mode: 'history',
@@ -16,10 +29,12 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: () => import('./views/About.vue'),
+    },
+    {
+      path: '/idepre',
+      name: 'idepre',
+      component:  () => import('./views/Idepre.vue'),
     },
   ],
 });
